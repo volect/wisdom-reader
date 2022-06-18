9065a12bc64c21bac311a07588a5afd44880dd13
abilops <abilops@users.noreply.github.com>
2022-06-18 12:47:32 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+    document.getElementById("javascriptWarning").remove();
+    b = document.getElementById("billboard");
+    t = document.getElementById("billboardH1");
+    c = document.getElementById("configPanel");
+
+    if(!localStorage.getItem('wiselines')) {
+        b.style.display="none";
+        c.style.display="block";
+         } 
+         else {
+            text = localStorage.getItem('wiselines');
+            lines = text.split("\n");
+            n = getRandomInt(0,lines.length);
+            t.innerText = lines[n]; 
+
+            c.style.display="none";
+            b.style.display="block";
+            document.getElementsByClassName("title")[0].innerText="";
+            document.getElementsByClassName("header")[0].style.display="none";
+        }
+}
+
+function saveLines() {
+    l = document.getElementById("wiselinesTextArea").value;
+    localStorage.setItem('wiselines', l);
+    document.location.reload();
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
+} 
